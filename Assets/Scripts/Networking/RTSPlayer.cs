51eb1b49b94eb756fc1c03de9b4d4a54662b4028
Martin Fučík <mar.fucik@gmail.com>
2021-03-18 12:40:07 +0100

    Storing Our Units

---
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class RTSPlayer : NetworkBehaviour
+{
+    [SerializeField] private List<Unit> myUnits = new List<Unit>();
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        Unit.ServerOnUnitSpawn += ServerHandleUnitSpawn;
+        Unit.ServerOnUnitDespawn += ServerHandleUnitDespawn;
+    }
+
+    public override void OnStopServer()
+    {
+        Unit.ServerOnUnitSpawn -= ServerHandleUnitSpawn;
+        Unit.ServerOnUnitDespawn -= ServerHandleUnitDespawn;
+    }
+
+    private void ServerHandleUnitSpawn(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        myUnits.Add(unit);
+    }
+
+    private void ServerHandleUnitDespawn(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        myUnits.Remove(unit);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly) { return; }
+
+        Unit.AuthorityOnUnitSpawn += AuthorityHandleUnitSpawn;
+        Unit.AuthorityOnUnitDespawn += AuthorityHandleUnitDespawn;
+    } 
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly) { return; }
+
+        Unit.AuthorityOnUnitSpawn -= AuthorityHandleUnitSpawn;
+        Unit.AuthorityOnUnitDespawn -= AuthorityHandleUnitDespawn;
+    }
+
+    private void AuthorityHandleUnitSpawn(Unit unit)
+    {
+        if (!hasAuthority) { return; }
+
+        myUnits.Add(unit);
+    }
+
+    private void AuthorityHandleUnitDespawn(Unit unit)
+    {
+        if (!hasAuthority) { return; }
+
+        myUnits.Remove(unit);
+    }
+
+    #endregion
+}
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using Mirror;
 using UnityEngine;
 using UnityEngine.Events;
@@ -10,12 +9,46 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
 
+    public static event Action<Unit> ServerOnUnitSpawn;
+    public static event Action<Unit> ServerOnUnitDespawn;
+
+    public static event Action<Unit> AuthorityOnUnitSpawn;
+    public static event Action<Unit> AuthorityOnUnitDespawn;
+
     public UnitMovement GetUnitMovement()
     {
         return unitMovement;
     }
 
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawn?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawn?.Invoke(this);
+    }
+
+    #endregion
+
     #region Client
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+
+        AuthorityOnUnitSpawn?.Invoke(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+
+        AuthorityOnUnitDespawn?.Invoke(this);
+    }
 
     [Client]
     public void Select() 
